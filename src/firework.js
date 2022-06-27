36472ea7a1a9267023b838b97b4e796780963f47
gustavsDev <gustavs.slaviets@gmail.com>
2022-06-27 19:19:21 +0300

    Second Commit

--- src/firework.js
+++ src/firework.js
@@ -4,6 +4,7 @@ class Firework {
 		this.vel = createVector(velx, vely);
 		this.color = color;
 		this.life = 0;
+		this.max_life = round(random(90, 105));
 		this.exploded = false;
 
 		this.vel.normalize().mult(3);
@@ -17,7 +18,7 @@ class Firework {
 
 	update() {
 		if(this.exploded) return;
-		if(this.life > 100) this.exploded = true;
+		if(this.life > this.max_life) this.exploded = true;
 		this.life++;
 		this.vel.rotate(random(-0.05, 0.05));
 		this.vel.y -= 0.06;
